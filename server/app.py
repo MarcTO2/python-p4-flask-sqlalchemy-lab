025e2ae6ec50c8fb969b904cfa7443e63334c09a
Absalom <mrclabsalom@gmail.com>
2023-09-28 12:25:50 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
-
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
@@ -10,7 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/')
@@ -19,16 +17,44 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    response_body = f'<ul>ID: {animal.id}</ul>'
+    response_body += f'<ul>Name: {animal.name}</ul>'
+    response_body += f'<ul>Species: {animal.species}</ul>'
+    response_body += '<ul>Zookeeper:</ul>'
+    response_body += f'<ul>Name: {animal.zookeeper.name}</ul>'
+    response_body += f'<ul>Birthday: {animal.zookeeper.birthday}</ul>'
+    response_body += '<ul>Enclosure:</ul>'
+    response_body += f'<ul>Environment: {animal.enclosure.environment}</ul>'
+    response_body += f'<ul>Open to Visitors: {animal.enclosure.open_to_visitors}</ul>'
+
+    return make_response(response_body)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    response_body = f'<ul>ID: {zookeeper.id}</ul>'
+    response_body += f'<ul>Name: {zookeeper.name}</ul>'
+    response_body += f'<ul>Birthday: {zookeeper.birthday}</ul>'
+    response_body += '<ul>Animals:</ul>'
+    for animal in zookeeper.animals:
+        response_body += f'<ul>Name: {animal.name}</ul>'
+        response_body += f'<ul>Species: {animal.species}</ul>'
+
+    return make_response(response_body)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body = f'<ul>ID: {enclosure.id}</ul>'
+    response_body += f'<ul>Environment: {enclosure.environment}</ul>'
+    response_body += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
+    response_body += '<ul>Animals:</ul>'
+    for animal in enclosure.animals:
+        response_body += f'<ul>Name: {animal.name}</ul>'
+        response_body += f'<ul>Species: {animal.species}</ul>'
 
+    return make_response(response_body)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
